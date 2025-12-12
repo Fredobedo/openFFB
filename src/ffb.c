0982a893c2beba6dbca570d8861c43e0d2f5673b
fredobedo <fbernard1@hotmail.com>
2025-12-12 09:20:23 +0100

    invert wheel position correction

--- src/ffb.c
+++ src/ffb.c
@@ -114,22 +114,19 @@ FFBStatus processPacket(unsigned char* packet)
 			if (getConfig()->SendWheelPositionToMidi==1)
 			{
 				int tempPosition=GetWheelPosition();
-				debug(3, "FRED DEBUG1");
-				if (tempPosition>-1){
-					debug(3, "FRED DEBUG2");
+				int FinalwheelPosition;
+				
+				if (tempPosition>-1)
 					wheelPosition=tempPosition;
-				}
 
 				debug(3, "GET_WHEEL_POSITION: %d\n", wheelPosition);
 				
 				if(getConfig()->InvertedWheelPosition==1)
-				{
-					debug(3, "FRED DEBUG3");
-					wheelPosition=16384 - wheelPosition;
-				}
-debug(3, "FRED DEBUG4");
-				replyPacket[1]=(wheelPosition >> 7) & 0x7f;
-				replyPacket[2]=wheelPosition & 0x7f;
+					FinalwheelPosition=16384 - wheelPosition;
+
+
+				replyPacket[1]=(FinalwheelPosition >> 7) & 0x7f;
+				replyPacket[2]=FinalwheelPosition & 0x7f;
 				replyPacket[3]=(replyPacket[0] ^ replyPacket[1] ^ replyPacket[2]) & 0x7f;
 			}
 			else
