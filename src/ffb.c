84a1be474f15825591737c16a75a9c29f56e6d44
fredobedo <fbernard1@hotmail.com>
2026-03-15 21:43:39 +0100

    test

--- src/ffb.c
+++ src/ffb.c
@@ -195,27 +195,27 @@ FFBStatus processPacket(unsigned char *packet)
 		inputPacket.crc = packet[7];
 
 		/* --- spring            from 0x00 to 0x7F -> 128 levels --- */
-		if (packet[1] == 0x0)
-			FFBStopEffect(ffb_effects[spring_effect_idx].id);
-		else if(previous_rawpacket[1] != packet[1])
-			FFBTriggerSpringEffect(previous_rawpacket[1] != packet[1], inputPacket.spring);
+		// if (packet[1] == 0x0)
+		// 	FFBStopEffect(ffb_effects[spring_effect_idx].id);
+		// else 
+			if(previous_rawpacket[1] != packet[1])
+				FFBTriggerSpringEffect(previous_rawpacket[1] != packet[1], inputPacket.spring);
 
 		/* --- friction          from 0x00 to 0x7F -> 128 levels                                                --- */
 		/* --- For now on, I will use Sine effect instead as I can't control the strengh of a froction effect ? --- */
-		if (packet[2] == 0x0)
-			FFBStopEffect(ffb_effects[friction_effect_idx].id);
-		else if(previous_rawpacket[2] != packet[2])
-			FFBTriggerFrictionEffect(previous_rawpacket[2] != packet[2], inputPacket.friction);
+		// if (packet[2] == 0x0)
+		// 	FFBStopEffect(ffb_effects[friction_effect_idx].id);
+		// else 
+			if(previous_rawpacket[2] != packet[2])
+				FFBTriggerFrictionEffect(previous_rawpacket[2] != packet[2], inputPacket.friction);
 
 		/* --- torqueDirection   0x00 = Left, 0x01  = Right                     --- */
 		/* --- torquePower       from 0x00 to 0x7F -> 128 levels                --- */
 		/* note that torqueDirection is where the wheel is turning                  */
-		if (packet[4] == 0x0)
-		{
-			FFBStopEffect(ffb_effects[constant_effect_idx].id);
-		}
-		else
-		{
+		// if (packet[4] == 0x0)
+		// 	FFBStopEffect(ffb_effects[constant_effect_idx].id);
+		// else
+		// {
 			if(previous_rawpacket[3] != packet[3] || previous_rawpacket[4] != packet[4])
 			{
 				if (inputPacket.torqueDirection == 0)
@@ -223,12 +223,13 @@ FFBStatus processPacket(unsigned char *packet)
 				else
 					FFBTriggerConstantEffect(previous_rawpacket[3] != packet[3] || previous_rawpacket[4] != packet[4], inputPacket.torquePower);
 			}
-		}
+		//}
 
-		if (packet[6] == 0x0 || packet[5] == 0x0)
-			FFBStopEffect(ffb_effects[sine_effect_idx].id);
-		else if(previous_rawpacket[5] != packet[5] || previous_rawpacket[6] != packet[6])
-			FFBTriggerSineEffect(previous_rawpacket[5] != packet[5] || previous_rawpacket[6] != packet[6], inputPacket.sineFrequency, inputPacket.sineIntensity);
+		// if (packet[6] == 0x0 || packet[5] == 0x0)
+		// 	FFBStopEffect(ffb_effects[sine_effect_idx].id);
+		// else 
+			if(previous_rawpacket[5] != packet[5] || previous_rawpacket[6] != packet[6])
+				FFBTriggerSineEffect(previous_rawpacket[5] != packet[5] || previous_rawpacket[6] != packet[6], inputPacket.sineFrequency, inputPacket.sineIntensity);
 
 		/* only copy if there is a diff */
 		if (memcmp(previous_rawpacket, packet, 8) != 0)
