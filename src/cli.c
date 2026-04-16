990207f06854bd8d4d0b58a0ce5023858f7b9056
fredobedo <fbernard1@hotmail.com>
2026-04-16 17:49:36 +0200

    test

--- src/cli.c
+++ src/cli.c
@@ -162,7 +162,7 @@ void SleepFromInput(char* input) {
                     
                     if (val >= 1 && val <= 60000) {
                         sleep_duration = (int)val;
-                        //printf("> Sleep: %d\n", sleep_duration);
+                        printf("> Sleep: %d\n", sleep_duration);
                         usleep(sleep_duration * 1000); // Convert ms to us
                     } else {
                         fprintf(stderr, "Error: Sleep duration must be 1-60000 milliseconds.\n");
@@ -237,7 +237,7 @@ void runInteractiveMode() {
                 break;
             } 
             else {    
-                //printf("> Sent: %02X %02X %02X %02X\n", packet[0], packet[1], packet[2], packet[3]);
+                printf("> Sent: %02X %02X %02X %02X\n", packet[0], packet[1], packet[2], packet[3]);
             }
         }
     }
