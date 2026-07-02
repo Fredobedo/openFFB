94e30437a474547e99169a249092b499bd98f82f
fredobedo <fbernard1@hotmail.com>
2026-07-02 14:49:05 +0200

    test

--- src/ffb.c
+++ src/ffb.c
@@ -145,7 +145,7 @@ FFBStatus processPacket(unsigned char *packet)
 		// 0xA1
 		case OPENFFB_SET_MAX_RIGHT_SUB_CMD: // Asynchronous command, reply will be sent by Worker thread
 			ThreadParams *maxRightParams = malloc(sizeof(ThreadParams));
-			*maxRightParams = (ThreadParams){0, 0.15, 0, 60000};
+			*maxRightParams = (ThreadParams){0, 0.20, 0, 60000};
 			startWorkerAsync(WorkerSetPosition, maxRightParams);
 			break;
 		// 0xA2
